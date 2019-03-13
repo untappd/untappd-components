67f738ba5680eea9ba386890a475338d703cd599
Drew Hamlett <i@drewh.me>
2019-03-13 14:28:05 -0400

    Updated the scoped styling of toggle

--- src/Toggle/index.js
+++ src/Toggle/index.js
@@ -1,40 +1,42 @@
 import React from 'react'
 import styled from 'styled-components'
 import Switch from 'react-switch'
-import Base from '../Base'
 import { getColor } from '../utils'
 
-const StyledToggle = styled(Base)`
-  .active .react-switch-bg {
+const lightBlue = 'rgba(30, 145, 234, 0.14)'
+
+function Toggle({ className, checked, ...props }) {
+  return (
+    <Switch
+      checked={checked}
+      uncheckedIcon={false}
+      checkedIcon={false}
+      className={`${className} ${checked ? 'active' : 'not-active'}`}
+      {...props}
+    />
+  )
+}
+
+const StyledToggle = styled(Toggle)`
+  &.active .react-switch-bg {
     box-sizing: border-box;
-    border: 1px solid ${getColor('darkBlue')};
-    background: rgba(30, 145, 234, 0.14) !important;
+    border: 1px solid ${getColor('blue')};
+    background: ${lightBlue} !important;
   }
 
-  .active .react-switch-handle {
-    background: ${getColor('darkBlue')} !important;
+  &.active .react-switch-handle {
+    background: ${getColor('blue')} !important;
   }
 
-  .not-active .react-switch-bg {
+  &.not-active .react-switch-bg {
     box-sizing: border-box;
     border: 1px solid ${getColor('grays.3')};
     background: ${getColor('grays.2')} !important;
   }
 
-  .not-active .react-switch-handle {
+  &.not-active .react-switch-handle {
     background: ${getColor('grays.3')} !important;
   }
 `
-export default function Toggle({ className, checked, ...props }) {
-  return (
-    <StyledToggle>
-      <Switch
-        checked={checked}
-        uncheckedIcon={false}
-        checkedIcon={false}
-        className={`${className} ${checked ? 'active' : 'not-active'}`}
-        {...props}
-      />
-    </StyledToggle>
-  )
-}
+
+export default StyledToggle
