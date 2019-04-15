58f7156ce322e42f8533e007fe8ea8dd686b398e
Zach Spoelstra <zspoelstra@gmail.com>
2019-04-15 08:51:36 -0400

    Need a quote there

--- cypress/integration/shapshot-spec.js
+++ cypress/integration/shapshot-spec.js
@@ -1,7 +1,7 @@
 describe('Overall page of components', () => {
   it('All components look right', () => {
     cy.visit('/')
-    cy.get('[data-cy="tooltip-example]').within(() => {
+    cy.get('[data-cy="tooltip-example"]').within(() => {
       cy.contains('Plain Text Example').trigger('mouseenter')
       cy.contains('Styled Text Example').trigger('mouseenter')
       cy.contains('Component Example').trigger('mouseenter')
