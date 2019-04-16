4fe199ae8c63f9b05ed5178150c1087e270f39a2
Zach Spoelstra <zspoelstra@gmail.com>
2019-04-16 09:28:50 -0400

    Ok, don’t show them

--- cypress/integration/shapshot-spec.js
+++ cypress/integration/shapshot-spec.js
@@ -1,11 +1,6 @@
 describe('Overall page of components', () => {
   it('All components look right', () => {
     cy.visit('/')
-    cy.get('[data-cy="tooltip-example"]').within(() => {
-      cy.contains('Plain Text Example').trigger('mouseenter')
-      cy.contains('Styled Text Example').trigger('mouseenter')
-      cy.contains('Component Example').trigger('mouseenter')
-    })
     cy.percySnapshot()
   })
 })
